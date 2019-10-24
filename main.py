1c5eef2f512676d8eff89f10f50108c65ea93538
Mikołaj Dłuś <mikolaj.dlus@gmail.com>
2019-10-24 10:33:30 +0200

    Converting path to absolute

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ def main():
 	arg_parser.add_argument('-d', '--dest', action='store', required=True, help='Path to destination directory')
 	arg_parser.add_argument('-v', '--verbose', action='store_true', help='Verbosity output')
 	args = arg_parser.parse_args()
+	args.dest = os.path.abspath(args.dest)
+	args.files_path = os.path.abspath(args.files_path)
 	if not os.path.isdir(args.dest):
 		os.mkdir(args.dest)
 	os.chdir(args.dest)
