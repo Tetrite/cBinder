cfb08bb693097f7bfd79c8a7d4e7045ee2157cdc
Dwoosh <34167272+Dwoosh@users.noreply.github.com>
2019-10-24 11:02:29 +0200

    Removed pycparser dependency closes #2
    Removed old code from scraper and  pycparser dependency

--- Scrapers.py
+++ Scrapers.py
@@ -1,5 +1,3 @@
-from pycparser import c_ast, parse_file
-import pathlib
 import CppHeaderParser
 
 
@@ -15,39 +13,10 @@ class DeclarationsScraper:
 		header = CppHeaderParser.CppHeader(filepath.name)
 		for fun in header.functions:
 			self.declarations.append(fun['debug'])
-		# if isinstance(filepath, pathlib.Path):
-		# 	filepath = filepath.as_posix()
-		# if self.cpp_path is None:
-		# 	ast = parse_file(filepath, use_cpp=True)
-		# else:
-		# 	ast = parse_file(filepath, use_cpp=True, cpp_path=self.cpp_path)
-		#
-		# self.declarations = [x for x in ast.ext if isinstance(x, c_ast.Decl)]
 
 	def parse_and_return_decl(self, filepath):
 		self.parse_file(filepath)
-		# return ' '.join([self.get_node_str(x) for x in self.declarations])
 		return ' '.join(self.declarations)
-
-	def print(self):
-		s = [self.get_node_str(x) for x in self.declarations]
-		print('\r\n'.join(s))
-
-	def get_node_str(self, node):
-		ret_str = self.get_decl(node)
-		args = [self.get_args_str(x) for x in node.type.args.params]
-		ret_str = ret_str + '(' + ', '.join(args) + ');'
-		return ret_str
-
-	def get_decl(self, node):
-		type_decl = node.type.type
-		return type_decl.type.names[0] + ' ' + type_decl.declname
-
-	def get_args_str(self, decl):
-		return self.get_args_type(decl) + ' ' + decl.name
-
-	def get_args_type(self, decl):
-		return decl.type.type.names[0]
 
 
 class IncludesScraper:
