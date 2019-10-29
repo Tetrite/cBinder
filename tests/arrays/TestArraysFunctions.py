6a058bdd43938e237434ed0f8f3bd5b161c22d88
Mateusz Monasterski <mateusz.mona@interia.pl>
2019-10-30 00:30:01 +0100

    Travis - different approach. With tests

--- tests/arrays/TestArraysFunctions.py
+++ tests/arrays/TestArraysFunctions.py
@@ -1,18 +1,23 @@
 import unittest
-import os
 import pathlib
-from tests.util.folder_clearing import clear_folder_contents
+import os
 
+from tests.util.folder_clearing import clear_folder_contents
+import nose
 class SimpleFunctionsTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.current_working_directory_path = pathlib.Path(os.getcwd())
+        self.current_working_directory_path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
         self.sources_path = self.current_working_directory_path.joinpath('sources')
         self.destination_path = self.current_working_directory_path.joinpath('generated')
+        print(self.current_working_directory_path)
+        print(self.destination_path)
+        print(self.sources_path)
         if not os.path.exists(str(self.destination_path)):
             os.makedirs(str(self.destination_path))
         clear_folder_contents(self.destination_path)
+        os.chdir(self.current_working_directory_path)
         os.chdir("../..")
         os.system(r'python main.py -f ' + str(self.sources_path) + ' -d ' + str(self.destination_path))
         os.chdir(self.destination_path)
@@ -20,7 +25,7 @@ class SimpleFunctionsTest(unittest.TestCase):
     def test_generate_bindings_to_function_with_array(self):
         self.fix_incorrect_import_in_wrapper()
 
-        from generated import arraytest
+        from tests.arrays.generated import arraytest
         a = []
         arraytest.func(a)
         self.assertEqual(a, [0.0, 1.0, 2.0, 3.0, 4.0])
@@ -28,7 +33,7 @@ class SimpleFunctionsTest(unittest.TestCase):
     def test_generate_bindings_to_function_with_array_sum(self):
         self.fix_incorrect_import_in_wrapper()
 
-        from generated import arraytest
+        from tests.arrays.generated import arraytest
         a = [2.0, 3.0]
         i = arraytest.sum(4.0, a)
         self.assertEqual(i, 9.0)
@@ -38,3 +43,7 @@ class SimpleFunctionsTest(unittest.TestCase):
             newText = f.read().replace('import _arraytest', 'from . import _arraytest')
         with open(str(self.destination_path.joinpath("arraytest.py")), "w") as f:
             f.write(newText)
+
+
+if __name__ == '__main__':
+    nose.run()
--- tests/simplefunctions/TestSimpleFunctions.py
+++ tests/simplefunctions/TestSimpleFunctions.py
@@ -3,33 +3,39 @@ import os
 import pathlib
 
 from tests.util.folder_clearing import clear_folder_contents
+import nose
 
 
 class SimpleFunctionsTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.current_working_directory_path = pathlib.Path(os.getcwd())
+        self.current_working_directory_path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
         self.sources_path = self.current_working_directory_path.joinpath('sources')
         self.destination_path = self.current_working_directory_path.joinpath('generated')
         if not os.path.exists(str(self.destination_path)):
             os.makedirs(str(self.destination_path))
         clear_folder_contents(self.destination_path)
+        os.chdir(self.current_working_directory_path)
         os.chdir("../..")
         os.system(r'python main.py -f ' + str(self.sources_path) + ' -d ' + str(self.destination_path))
         os.chdir(self.destination_path)
 
     def test_generate_bindings_to_simple_add_function_integer(self):
-        from generated import _add
+        from tests.simplefunctions.generated import _add
         sum = _add.lib.add(1, 2)
         self.assertEqual(sum, 3)
 
     def test_generate_bindings_to_simple_add_function_float(self):
-        from generated import _add
+        from tests.simplefunctions.generated import _add
         sum = _add.lib.addF(1.1, 2.2)
         self.assertAlmostEqual(sum, 3.3, places=6)
 
     def test_generate_bindings_to_simple_print_char_function(self):
-        from generated import _char
+        from tests.simplefunctions.generated import _char
         char = _char.lib.printChar('a'.encode('ascii'))
         self.assertEqual(char, 97)
+
+
+if __name__ == '__main__':
+    nose.run()
