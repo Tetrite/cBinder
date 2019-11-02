d887098a2cf3a6e92fb812352711074b10fe30d4
Mateusz Monasterski <mateusz.mona@interia.pl>
2019-11-02 14:34:34 +0100

    Created example test for doxygen parser

---
+++ tests/functionwithdoxygen/sources/ex_doxygen.c
@@ -0,0 +1,10 @@
+#include "ex_doxygen.h"
+int reverse_array_order(const int n, int* in_order, int reverse_order[]){
+
+    int j = n-1;
+	for(int i=0; i<n; i++){
+		reverse_order[i] = in_order[j];
+		j--;
+	}
+	return 0;
+}
