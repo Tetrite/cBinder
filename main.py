149ab44c38f545f15e6772c95c8afe8dbeea69a3
Mikołaj Dłuś <mikolaj.dlus@gmail.com>
2019-11-04 11:23:11 +0100

    Added new argument to hold paths to external libraries

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from BindingsGenerator import *
 
 def main():
 	arg_parser = argparse.ArgumentParser('Generate python bindings')
-	arg_parser.add_argument('-f', '--files_path', action='store', required=True, help='Path to C library')
-	arg_parser.add_argument('-d', '--dest', action='store', required=True, help='Path to destination directory')
+	arg_parser.add_argument('-f', '--files_path', action='store', required=True, help='Path to C library', metavar='path')
+	arg_parser.add_argument('-d', '--dest', action='store', required=True, help='Path to destination directory',
+																									metavar='path')
+	arg_parser.add_argument('-i', '--include', action='append', help='Path to external library to be include',
+																									metavar='path')
 	arg_parser.add_argument('-v', '--verbose', action='store_true', help='Verbosity output')
 	args = arg_parser.parse_args()
 	args.dest = os.path.abspath(args.dest)
