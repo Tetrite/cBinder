392bb18084a27f805b6f76396250b15fcd8e6560
Mikołaj Dłuś <mikolaj.dlus@gmail.com>
2019-11-12 15:41:00 +0100

    new class for dynamic library files

---
+++ LibraryFile.py
@@ -0,0 +1,29 @@
+import os
+import pathlib
+import platform
+
+
+class LibraryFile:
+	"""
+	Class representing shared/dynamic library file
+
+	Attributes
+	----------
+	filepath : Path
+		Path object pointing to source file
+	"""
+	def __init__(self, path):
+		self.filepath = path
+
+	def __str__(self):
+		return 'Source file path: ' + self.filepath.as_posix()
+
+
+def get_shared_library_files(dirpath):
+	library_ext = '.so' if platform.system() == 'Linux' else '.dll'
+	libraries = []
+	for path, subdirs, files in os.walk(dirpath):
+		for name in files:
+			if name.endswith(library_ext):
+				libraries.append(LibraryFile(pathlib.Path(path, name)))
+	return libraries
