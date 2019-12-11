b9c141ccb21322b06ce0bdf31ded1d7dcc7c9965
Mateusz Monasterski <mateusz.mona@interia.pl>
2019-12-11 18:44:08 +0100

    Bug with CppHeaderParser - added test to be fixed

---
+++ tests/withdoxygen/cppheaderparser_err/sources/parser_brief_err.h
@@ -0,0 +1,12 @@
+/**
+ * @brief CppHeaderParser brief error
+ */
+
+/**
+  * @param[in]   a          first number
+  * @param[in]   b          second number
+  * @return                sum
+  */
+
+int sum(int a, int b);
+
