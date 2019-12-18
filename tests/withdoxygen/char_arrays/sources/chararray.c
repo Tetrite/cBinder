cc98a82738f63561df45f02d63ea288f629ae6db
Mateusz Monasterski <mateusz.mona@interia.pl>
2019-12-18 01:15:04 +0100

    Added test for char arrays passing

---
+++ tests/withdoxygen/char_arrays/sources/chararray.c
@@ -0,0 +1,30 @@
+#include "chararray.h"
+#include <string.h>
+
+int get_string_length_pointer(char *the_string){
+    return strlen(the_string);
+}
+
+int get_string_length_braces(char the_string[]){
+    return strlen(the_string);
+}
+
+int get_sum_of_string_lengths_pointers(char **the_string){
+    int sum=0;
+    int i = 0;
+    while (the_string[i] != NULL){
+        sum += strlen(the_string[i]);
+        i++;
+    }
+    return sum;
+}
+
+int get_sum_of_string_lengths_mix(char *the_string[]){
+    int sum=0;
+    int i = 0;
+    while (the_string[i] != NULL){
+        sum += strlen(the_string[i]);
+        i++;
+    }
+    return sum;
+}
---
+++ tests/withdoxygen/char_arrays/sources/chararray.h
@@ -0,0 +1,27 @@
+/**
+  * @param[in]  the_string    char array
+  * @return                length
+  */
+
+int get_string_length_pointer(char *the_string);
+
+/**
+  * @param[in]  the_string    char array
+  * @return                length
+  */
+
+int get_string_length_braces(char the_string[]);
+
+/**
+  * @param[in]  the_string    char array
+  * @return                length
+  */
+
+int get_sum_of_string_lengths_pointers(char **the_string);
+
+/**
+  * @param[in]  the_string    char array
+  * @return                length
+  */
+
+int get_sum_of_string_lengths_mix(char *the_string[]);
