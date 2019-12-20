7343d501a80eeccefc3683331dbc8ae737a92b91
Wojciech Basiura <basiura.wojciech@gmail.com>
2019-12-20 01:56:15 +0100

    First cBinder release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="cBinder",
-    version="0.1",
+    version="1.0",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
 
     install_requires=[
